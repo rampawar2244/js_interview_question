56e341e47eeaa478693c1ec2169ffb94f96cadeb
ashwin-dongare <ashwin.dongare@studiolabs.com>
2023-12-31 01:07:01 -0800

    2 question solved

---
+++ script.js
@@ -0,0 +1,23 @@
+// Q Factorial in js
+// function fact(n){
+//     if(n === 0){
+//         return 1
+//     }else{
+//         return n * fact(n - 1)
+//     }
+// }
+// console.log(fact(5))
+
+// Q string is palindrome or not
+ 
+// function isPalindrome(str) {
+//     str = str.toLowerCase() 
+//     let reverseStr = str.split('').reverse().join('')
+//     return str === reverseStr
+// }
+ 
+
+// console.log(isPalindrome("aba"));
+
+// Q Reverse Each and Every word in string
+
